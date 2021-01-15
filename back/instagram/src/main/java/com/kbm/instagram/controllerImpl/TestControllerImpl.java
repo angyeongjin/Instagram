25a398dde1de3a1e5e21f2f58874313a9dcc1aba
안경진 <akj0426@gmail.com>
2021-01-15 17:19:47 +0900

    [test] 테스트 코드 수정
    테스트 컨트롤러 인터페이스 상속 빠진거 넣음

--- back/instagram/src/main/java/com/kbm/instagram/controllerImpl/TestControllerImpl.java
+++ back/instagram/src/main/java/com/kbm/instagram/controllerImpl/TestControllerImpl.java
@@ -1,11 +1,12 @@
 package com.kbm.instagram.controllerImpl;
 
+import com.kbm.instagram.controller.TestController;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/test")
 @CrossOrigin(origins = { "*" }, maxAge = 6000)
-public class TestControllerImpl {
+public class TestControllerImpl implements TestController {
 
     @GetMapping("/getMsg")
     @ResponseBody
