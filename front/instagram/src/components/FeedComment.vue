f2dfd047c8bf647abcd1625f16ce63a5afebc893
MinJeong <pacedov3@gmail.com>
2021-01-22 15:06:55 +0900

    [fix] main 페이지에 사용된 components 세분화
    다른 페이지에서도 사용되는 댓글, 좋아요 부분 컴포넌트화
    
    - FeedComment Component
    - AddFeedComment Component
    - FeedUtil Component

---
+++ front/instagram/src/components/FeedComment.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="feed-comment">
+    <a href="#" class="nick-name">lee.ddd05</a>
+    <p style="display: inline;">
+      피드가 제 마음에 쏙 드네용~~^^
+    </p>
+  </div>
+  <div class="feed-comment">
+    <a href="#" class="nick-name">lee.ddd05</a>
+    <p style="display: inline;">
+      안녕하세요 반갑습니닷 사진보고 저도 모르게 하트를 누르고 말았네요~~♥
+    </p>
+  </div>
+  <span style="font-size: 10px;letter-spacing: .2px;color: #8e8e8e;"
+    >2일전</span
+  >
+</template>
+<script>
+export default {};
+</script>
