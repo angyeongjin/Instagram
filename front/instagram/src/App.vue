7b630c7a550bc6390cfc2edcdc48b7387ae87ea0
MinJeong <pacedov3@gmail.com>
2021-01-29 18:38:01 +0900

    [fix] css 수정

--- front/instagram/src/App.vue
+++ front/instagram/src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <div id="wrap">
-    <insta-header v-if="this.$store.getters.token" />
-    <router-view />
-  </div>
+  <!-- <div id="wrap">
+    <insta-header v-if="this.$store.getters.token" /> -->
+  <router-view />
+  <!-- </div> -->
 </template>
 <script>
 import InstaHeader from "@/components/Header.vue";
