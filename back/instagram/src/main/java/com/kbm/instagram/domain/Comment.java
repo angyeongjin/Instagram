6e5ab6367ac29fc7d5486aebf5d36fe1dd0ebd00
sebong Oh <osb4226@gmail.com>
2021-02-16 12:59:46 +0900

    [add] Comment domain

---
+++ back/instagram/src/main/java/com/kbm/instagram/domain/Comment.java
@@ -0,0 +1,27 @@
+package com.kbm.instagram.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Comment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id; //기본키
+
+    @ManyToOne
+    private Member writer;
+
+    private String content; //글
+
+}
