3c67ffd690c0afb0b08ebe7254a1d22a305831f1
sebong Oh <osb4226@gmail.com>
2021-02-17 18:47:41 +0900

    [add] CommentService - update, delete , findByid

--- back/instagram/src/main/java/com/kbm/instagram/service/CommentService.java
+++ back/instagram/src/main/java/com/kbm/instagram/service/CommentService.java
@@ -4,6 +4,10 @@ package com.kbm.instagram.service;
 import com.kbm.instagram.dto.CommentDto;
 
 public interface CommentService {
+
     public CommentDto create(CommentDto commentDto);
+    public CommentDto update(CommentDto commentDto);
+    public void delete(Long id);
+    public CommentDto findById(Long id);
 
 }
