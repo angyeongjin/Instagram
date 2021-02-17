c89072cf63033b7f07355d8252348c105f257691
MinJeong <pacedov3@gmail.com>
2021-02-17 21:44:43 +0900

    [fix] main component
    - folder 변경
    - FeedItem Component 바인딩 수정

---
+++ front/instagram/src/components/main/FeedContents.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="feed-contents">
+    <a href="#" class="nick-name">Lorem Ipsum</a>
+    <p style="display: inline;">
+      {{ content }}
+      <br />
+      <br />
+      <a href="#" class="tag">#lorem Ipsum</a>
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  props: ["content"]
+};
+</script>
