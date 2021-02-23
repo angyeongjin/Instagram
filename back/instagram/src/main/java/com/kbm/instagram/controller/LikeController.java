a5779d7af1f125f8da7f602f71f57eed5f8abd6e
안경진 <akj0426@gmail.com>
2021-02-23 13:51:44 +0900

    [fix] RequestLikeDto 추가

--- back/instagram/src/main/java/com/kbm/instagram/controller/LikeController.java
+++ back/instagram/src/main/java/com/kbm/instagram/controller/LikeController.java
@@ -2,6 +2,7 @@ package com.kbm.instagram.controller;
 
 import com.kbm.instagram.dto.FeedDto;
 import com.kbm.instagram.dto.MemberDto;
+import com.kbm.instagram.dto.RequestLikeDto;
 import com.kbm.instagram.service.FeedService;
 import com.kbm.instagram.service.LikeService;
 import com.kbm.instagram.service.MemberService;
@@ -23,8 +24,9 @@ public class LikeController {
 
     @PostMapping
     @ApiOperation(value = "좋아요/좋아요 취소하기", notes = "해당 피드에 좋아요를 누릅니다. 이미 좋아요가 되어있는 경우 좋아요를 취소합니다.")
-    public List<MemberDto> like (@RequestBody FeedDto feedDto) {
+    public List<MemberDto> like (@RequestBody RequestLikeDto requestLikeDto) {
         MemberDto memberDto = memberService.getAuthMember();
+        FeedDto feedDto = feedService.findByFeedId(requestLikeDto.getFeedId());
         List<MemberDto> likeList = null;
         try {
          likeList = likeService.like(feedDto, memberDto);
