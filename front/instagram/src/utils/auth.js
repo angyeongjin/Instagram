b18f43172c37b795a2fec46a0294bc98329be18d
MinJeong <pacedov3@gmail.com>
2021-03-02 17:22:30 +0900

    [fix] vuex 새로고침 이슈 해결 #31

--- front/instagram/src/utils/auth.js
+++ front/instagram/src/utils/auth.js
@@ -7,3 +7,5 @@ export const getToken = () => Cookies.get(TokenKey);
 export const setToken = token => Cookies.set(TokenKey, token);
 
 export const removeToken = () => Cookies.remove(TokenKey);
+
+export const userInfo = JSON.parse(sessionStorage.getItem("member"));
